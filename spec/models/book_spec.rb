f7da148e41d7712298216c13e3cb3f4a2893ccf5
Allie Sterling <sterlina89@gmail.com>
2015-02-02 11:11:23 -0800

    tests that you can call book.subjects

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Book, :type => :model do
     end
   end
 
+  describe 'has_many subjects' do
+
+    it 'allows you to call book.subjects' do
+      book = Book.create(title: "blah")
+      subject = Subject.create(name: "Fiction")
+      subject2 = Subject.create(name: "Horror")
+      BookSubject.create(book_id: book.id, subject_id: subject.id)
+      BookSubject.create(book_id: book.id, subject_id: subject2.id)
+      expect(book.subjects.count).to eq 2
+
+    end
+  end
+
 end
