1e026f646b103fc28963935447dcd97e682a5af7
Allie Sterling <sterlina89@gmail.com>
2015-02-02 10:17:38 -0800

    fixes test for get show

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe BooksController do
+  let(:book) {Book.create(title: "The Great Gatsby")}
 
   describe 'GET #index' do
-    let(:book) {Book.create(title: "The Great Gatsby")}
     it 'is successful' do
       get :index
       expect(response.status).to eq 200
@@ -17,9 +17,17 @@ describe BooksController do
 
   describe 'GET #show' do
     it 'is successful' do
-      get :show, id: 1
+      get :show, id: book.id
       expect(response.status).to eq 200
     end
+  end
+
+  describe 'GET #search' do
+    it 'is successful' do
+      get :search, title: 'stars'
+      expect(response.status).to eq 200
+    end
+
   end
 
 end
