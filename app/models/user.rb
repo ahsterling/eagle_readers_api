25de542aae96df4602bcc423a031c2c92403eecb
Allie Sterling <sterlina89@gmail.com>
2015-02-03 09:55:34 -0800

    tiny amounts of refactoring for the has_genre_badge method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules.
   validates :email, presence: true
   has_many :user_books
   has_many :books, through: :user_books
@@ -7,15 +6,15 @@ class User < ActiveRecord::Base
   has_many :genre_badges, through: :user_genre_badges
 
   def has_genre_badge?(genre_name)
-    has_book = false
-    self.books.each do |book|
+    has_badge = false
+    self.books.includes(:subjects).each do |book|
       book.subjects.each do |subject|
         if subject.name == genre_name
-          has_book = true
+          has_badge = true
         end
       end
     end
-    return has_book
+    return has_badge
   end
 
 end
