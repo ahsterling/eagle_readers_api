1a1c6c650cc176ccc8a9b5400ce0b97055a638b4
Allie Sterling <sterlina89@gmail.com>
2015-02-09 16:45:09 -0800

    remove extra things from deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,17 +40,8 @@ set :deploy_to, '/var/www/eagle-readers-api'
 
 namespace :deploy do
 
-  desc 'Restart application'
-
-  task :restart do
-    on roles(:web), in: :sequence, wait: 5 do
-      execute :mkdir, '-p', "#{ release_path }/tmp"
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    on roles(:web, :app, :db), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
