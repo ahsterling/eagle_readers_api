4c85f9ff381356a97c5be6dc20f2c85bcc0e7b76
Allie Sterling <sterlina89@gmail.com>
2015-02-09 16:36:00 -0800

    more changes to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,10 +40,17 @@ set :deploy_to, '/var/www/eagle-readers-api'
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+  desc 'Restart application'
+
+  task :restart do
+    on roles(:app, :web, :db), in: :sequence, wait: 5 do
       execute :mkdir, '-p', "#{ release_path }/tmp"
       execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
