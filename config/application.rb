8205d4b1a1bfe13186bdbe9c5944c1fbebc03ffc
Allie Sterling <sterlina89@gmail.com>
2015-02-12 15:24:47 -0800

    changes cors settings

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module EagleReadersApi
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins '*'
+        origins 'http://localhost:8000'
         resource '*',
                  :headers => :any,
                  :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
