e79a73312a8b5b2cf77781a903785e19b7b27de0
Allie Sterling <sterlina89@gmail.com>
2015-02-12 14:48:52 -0800

    adds angular elastic app to application.rb for cors support

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module EagleReadersApi
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins 'localhost:8000'
+        origins 'localhost:8000', 'http://54.186.52.32'
         resource '*',
                  :headers => :any,
                  :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
