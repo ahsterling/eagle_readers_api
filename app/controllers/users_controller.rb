7666556e74d985dbff292296fbd5b728e3033077
Allie Sterling <sterlina89@gmail.com>
2015-02-16 13:04:57 -0800

    adds tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,28 +22,39 @@ class UsersController < ApplicationController
   def add_book
     UserBook.create(user_id: params[:user_id], book_id: params[:book_id])
     user = User.find(params[:user_id])
+    badges_earned = []
     GenreBadge.all.each do |badge|
 
       unless user.already_has_badge(badge.id)
       # check for bulk badges
         if badge.bulk_badge == true
           if user.has_bulk_genre_badge?(badge.genre_name)
-            UserGenreBadge.create(user_id: user.id, genre_badge_id: badge.id)
+            bulk_badge = UserGenreBadge.create(user_id: user.id, genre_badge_id: badge.id)
+            badges_earned << bulk_badge
           end
         # check for explorer badges
         elsif badge.explorer_badge == true
           if user.has_genre_explorer_badge?
-            UserGenreBadge.create(user_id: user.id, genre_badge_id: badge.id)
+            explorer_badge = UserGenreBadge.create(user_id: user.id, genre_badge_id: badge.id)
+            badges_earned << explorer_badge
           end
         # check for standard genre badges
         elsif user.has_genre_badge?(badge.genre_name)
-          UserGenreBadge.create(user_id: user.id, genre_badge_id: badge.id)
+          badge = UserGenreBadge.create(user_id: user.id, genre_badge_id: badge.id)
+          badges_earned << badge
         end
       end
     end
+    book = Book.find(params[:book_id])
+    response = {
+      book: book.title,
+      badges: []
+    }
+    badges_earned.each do |badge|
+      response[:badges] << badge.genre_badge.title
+    end
 
-    @book = Book.find(params[:book_id])
-    render json: @book.as_json
+    render json: response.as_json
   end
 
   def usernames
