84af8fce5df235951390d7fa8c302a675229e013
Allie Sterling <sterlina89@gmail.com>
2015-02-19 14:11:02 -0800

    adds action controller request forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::Helpers
   include ActionController::Cookies
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionController::RequestForgeryProtection
+  # protect_from_forgery with: :exception
+
 end
