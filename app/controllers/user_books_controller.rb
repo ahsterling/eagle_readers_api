0b5bf08c3c82208e1d8235ab1633cff01b23ef09
Allie Sterling <sterlina89@gmail.com>
2015-02-20 11:46:23 -0800

    makes user id and book id integers in user books destroy method

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,7 +1,7 @@
 class UserBooksController < ApplicationController
 
   def destroy
-    user_book = UserBook.find_by(user_id: params[:user_id], book_id: params[:book_id])
+    user_book = UserBook.find_by(user_id: params[:user_id].to_i, book_id: params[:book_id].to_i)
     user_book.destroy
   end
 end
