6fbd8b680ec87f9ab5eb07ba01d2e5e5fca490ba
Allie Sterling <sterlina89@gmail.com>
2015-02-20 13:38:20 -0800

    passes test for destroying genre badge when book is deleted

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -3,5 +3,12 @@ class UserBooksController < ApplicationController
   def destroy
     user_book = UserBook.find_by(user_id: params[:user_id].to_i, book_id: params[:book_id].to_i)
     user_book.destroy
+    user = User.find(params[:user_id])
+    book = Book.find(params[:book_id])
+    unless user.has_genre_badge?(book.genre.name)
+      genre_badge = GenreBadge.find_by(genre_name: book.genre.name)
+      puts genre_badge.id
+      UserGenreBadge.find_by(user_id: user.id, genre_badge_id: genre_badge.id).destroy
+    end
   end
 end
--- spec/controllers/user_books_controller_spec.rb
+++ spec/controllers/user_books_controller_spec.rb
@@ -7,11 +7,29 @@ describe UserBooksController do
       user = User.create(email: 'uniqueemail@email.com', password: "blahblah", password_confirmation: "blahblah", uid: "email@email.com")
       auth_headers = user.create_new_auth_token
       request.headers.merge!(auth_headers)
-
-      book = Book.create(title: "blah")
+      genre = Genre.create(name: "Fiction")
+      book = Book.create(title: "blah", genre_id: genre.id)
       user_book = UserBook.create(user_id: user.id, book_id: book.id)
+      genre_badge = GenreBadge.create(genre_name: "Fiction")
+      UserGenreBadge.create(genre_badge_id: genre_badge.id, user_id: user.id)
+
       delete :destroy, { user_id: user.id, book_id: book.id }
       expect(user.books.count).to eq 0
+
+    end
+
+    it 'removes the appropriate genre badges when user only has one book of that genre' do
+      user = User.create(email: 'uniqueemail2@email.com', password: "blahblah", password_confirmation: "blahblah", uid: "uniqueemail2@email.com")
+      auth_headers = user.create_new_auth_token
+      request.headers.merge!(auth_headers)
+      genre = Genre.create(name: "Fiction")
+      book = Book.create(title: "blah", genre_id: genre.id)
+      user_book = UserBook.create(user_id: user.id, book_id: book.id)
+      genre_badge = GenreBadge.create(genre_name: "Fiction")
+      UserGenreBadge.create(genre_badge_id: genre_badge.id, user_id: user.id)
+      expect(user.genre_badges.count).to eq 1
+      delete :destroy, {user_id: user.id, book_id: book.id}
+      expect(user.genre_badges.count).to eq 0
 
     end
 
